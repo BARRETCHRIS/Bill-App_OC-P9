689a4e09485912b6ba394f5816ccf2d4203320b4
BARRETCHRIS <christelle.barret.poirot@gmail.com>
2024-06-10 11:13:58 +0200

    Fix admin login bugs

--- Billed-app-FR-Front/src/containers/Login.js
+++ Billed-app-FR-Front/src/containers/Login.js
@@ -38,13 +38,21 @@ export default class Login {
   }
 
   handleSubmitAdmin = e => {
+    // Fix Login admin
     e.preventDefault()
     const user = {
       type: "Admin",
-      email: e.target.querySelector(`input[data-testid="employee-email-input"]`).value,
-      password: e.target.querySelector(`input[data-testid="employee-password-input"]`).value,
+      email: e.target.querySelector(`input[data-testid="admin-email-input"]`).value,
+      password: e.target.querySelector(`input[data-testid="admin-password-input"]`).value,
       status: "connected"
     }
+    // e.preventDefault()
+    // const user = {
+    //   type: "Admin",
+    //   email: e.target.querySelector(`input[data-testid="employee-email-input"]`).value,
+    //   password: e.target.querySelector(`input[data-testid="employee-password-input"]`).value,
+    //   status: "connected"
+    // }
     this.localStorage.setItem("user", JSON.stringify(user))
     this.login(user)
       .catch(
